8351b9f3ef5243441e49e969da96abe7181390e2
Selçuk Ermaya <selcukermaya@gmail.com>
2019-10-26 13:20:22 +0300

    Fix on the source code 😄

--- Controllers/RulesController.cs
+++ Controllers/RulesController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Amazon;
@@ -24,16 +25,20 @@ namespace AWSHelper.SecurityGroup.Controllers
 		[HttpPost]
 		public async Task<IActionResult> Post(IpModel model)
 		{
+			if (model == null) return BadRequest(new { Success = false, Message = "Invalid model." });
 			if (!Request.Headers.ContainsKey("Api-Secret")) return BadRequest(new { success = false, message = "Missing Secret header" });
 			var values = Request.Headers["Api-Secret"];
-			var apiSecret = Environment.GetEnvironmentVariable("AWS_API_SECRET");
+			var apiSecret = Environment.GetEnvironmentVariable("AWSHELPER_API_SECRET");
 			if (values.FirstOrDefault(x => x == apiSecret) == null) return Unauthorized(new { success = false, message = "Invalid secret header" });
 
-			var namePrefix = Environment.GetEnvironmentVariable("AWS_NAME_PREFIX");
-			var groupId = Environment.GetEnvironmentVariable("AWS_GROUP_ID");
-			var accessId = Environment.GetEnvironmentVariable("AWS_ACCESS_ID");
-			var accessSecret = Environment.GetEnvironmentVariable("AWS_ACCESS_SECRET");
-			var endpoint = RegionEndpoint.GetBySystemName(Environment.GetEnvironmentVariable("AWS_REGION"));
+
+			if (string.IsNullOrEmpty(model.Name) || string.IsNullOrWhiteSpace(model.Name)) return BadRequest(new { Success = false, Message = "Invalid name" });
+
+			var namePrefix = Environment.GetEnvironmentVariable("AWSHELPER_NAME_PREFIX");
+			var groupId = Environment.GetEnvironmentVariable("AWSHELPER_GROUP_ID");
+			var accessId = Environment.GetEnvironmentVariable("AWSHELPER_ACCESS_ID");
+			var accessSecret = Environment.GetEnvironmentVariable("AWSHELPER_ACCESS_SECRET");
+			var endpoint = RegionEndpoint.GetBySystemName(Environment.GetEnvironmentVariable("AWSHELPER_REGION"));
 			var client = new AmazonEC2Client(accessId, accessSecret, endpoint);
 
 			var req = new DescribeSecurityGroupsRequest();
@@ -44,23 +49,69 @@ namespace AWSHelper.SecurityGroup.Controllers
 				GroupId = groupId
 			};
 
-			var ipPermission = new IpPermission
+			try
 			{
-				FromPort = model.Port,
-				ToPort = model.Port,
-				IpProtocol = model.Protocol
-			};
+				var secGroupRequest = new DescribeSecurityGroupsRequest();
+				secGroupRequest.GroupIds.Add(groupId);
 
-			ipPermission.Ipv4Ranges.Add(new IpRange { CidrIp = $"{model.Ip}/32", Description = $"{namePrefix}{model.Name}" });
-			request.IpPermissions.Add(ipPermission);
+				var get = await client.DescribeSecurityGroupsAsync(secGroupRequest);
 
-			var result = await client.UpdateSecurityGroupRuleDescriptionsIngressAsync(request);
+				var securityGroup = get.SecurityGroups.FirstOrDefault();
+				if (securityGroup == null) return BadRequest(new { Success = false, Message = "Security group not found." });
 
-			return Ok(new
+				var description = $"{namePrefix}{model.Name}";
+
+				var ipRangeListRemoved = new List<IpRange>();
+				var ipRangeListAdded = new List<IpRange>();
+
+				foreach (var permission in securityGroup.IpPermissions)
+				{
+					foreach (var ipRange in permission.Ipv4Ranges)
+						if (description.Equals(ipRange.Description) && permission.ToPort == model.Port)
+							ipRangeListRemoved.Add(ipRange);
+					permission.Ipv4Ranges.RemoveAll(x => ipRangeListRemoved.Contains(x));
+				}
+
+				if (ipRangeListRemoved.Any())
+				{
+					var revokeRequest = new RevokeSecurityGroupIngressRequest();
+					revokeRequest.GroupId = groupId;
+					revokeRequest.IpPermissions.Add(new IpPermission
+					{
+						FromPort = model.Port,
+						IpProtocol = model.Protocol,
+						ToPort = model.Port,
+						Ipv4Ranges = new List<IpRange>(ipRangeListRemoved)
+					});
+					await client.RevokeSecurityGroupIngressAsync(revokeRequest);
+				}
+
+				var authorizeRequest = new AuthorizeSecurityGroupIngressRequest();
+				authorizeRequest.GroupId = groupId;
+				var authorizeIpRange = new IpRange
+				{
+					CidrIp = $"{model.Ip}/32",
+					Description = description
+				};
+				authorizeRequest.IpPermissions.Add(new IpPermission
+				{
+					FromPort = model.Port,
+					IpProtocol = model.Protocol,
+					ToPort = model.Port,
+					Ipv4Ranges = new List<IpRange> { authorizeIpRange }
+				});
+				var authorizeResult = await client.AuthorizeSecurityGroupIngressAsync(authorizeRequest);
+
+				return Ok(new
+				{
+					Success = true,
+					Message = "Action completed."
+				});
+			}
+			catch (Exception ex)
 			{
-				Success = result.HttpStatusCode == System.Net.HttpStatusCode.OK,
-				Message = "Action completed."
-			});
+				return BadRequest(new { Success = false, Message = ex.Message });
+			}
 		}
 	}
 }
